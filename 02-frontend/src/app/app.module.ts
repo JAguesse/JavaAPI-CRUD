d90608b776c90102533ce84e2eb213e0fa50b108
justinaguesse <45197420+JAguesse@users.noreply.github.com>
2022-05-30 17:30:38 +0200

    Initialisation GET method

--- 02-frontend/src/app/app.module.ts
+++ 02-frontend/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { RestaurantListComponent } from './components/restaurant-list/restaurant-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatTableModule } from '@angular/material/table';
 
 const routes: Routes = [
   
@@ -20,7 +21,10 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule, 
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule, 
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 02-frontend/src/app/services/restaurant.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Restaurant } from '../common/restaurant';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantService {
+
+
+  private baseUrl = "http://localhost:8080/restaurants";
+  
+  constructor(private httpClient: HttpClient) { }
+
+  getRestaurantList(): Observable<Restaurant[]>{
+    const searchUrl = `${this.baseUrl}`
+    return this.getRestaurants(searchUrl);
+  }
+  private getRestaurants(searchUrl: string): Observable<Restaurant[]>{
+    return this.httpClient.get<GetResponseRestaurants>(searchUrl).pipe(
+      map(response => response._embedded.restaurants)
+    );
+  }
+}
+
+
+interface GetResponseRestaurants {
+  _embedded: {
+    restaurants: Restaurant[];
+  }
+}
